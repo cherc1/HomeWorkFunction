e5f461e05fb864be273e4825269e9f2eb5f43bd6
Arina Skripova <skripovaarina1@gmail.com>
2023-12-07 15:29:24 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [size];
+Random rnd = new Random();
+for(int i = 0; i < size; i++)
+{
+    array[i] = rnd.Next(1, 10);
+    Console.Write($"{array[i]} ");
+}
+void FlipOver(int [] arr)
+{
+    int size2 = arr.Length;
+    int [] arr2 = new int[size2];
+    int i = 0;
+    while (i < size2)
+    {
+        arr2[i] = arr[size2 - i];
+    }
+    i++;
+    Console.Write($"Ответ: {arr2[i]} ");
+}
+FlipOver(array);
